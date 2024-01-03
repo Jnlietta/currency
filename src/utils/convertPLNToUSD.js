db21837d15c99a44877a550254235d9ea123e64f
Julia Glinka <julia.w.glinka@gmail.com>
2024-01-03 15:50:54 +0100

    Add in func convertPLNToUSD case if param is text

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,11 +1,16 @@
 export const convertPLNToUSD = (PLN) => {
 
-  const PLNtoUSD = PLN / 3.5;
-  
-  const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD'
-  });
+  if (typeof PLN === 'string' || PLN instanceof String) {
+    return NaN;
+  } else { 
 
-  return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
+    const PLNtoUSD = PLN / 3.5;
+    
+    const formatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    });
+
+    return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
+  }
 }
