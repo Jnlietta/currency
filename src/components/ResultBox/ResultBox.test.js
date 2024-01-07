790235e1b5a9701aa34a3d8f2336393c3e0f8221
Julia Glinka <julia.w.glinka@gmail.com>
2024-01-07 16:24:51 +0100

    Add test for rendering proper info about conversion when pln->usd  in ResultBox

--- src/components/ResultBox/ResultBox.test.js
+++ src/components/ResultBox/ResultBox.test.js
@@ -1,8 +1,22 @@
 import ResultBox from './ResultBox';
-import { render} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
 
 describe('Component ResultBox', () => {
     it('should render without crashing', () => {
         render(<ResultBox from="PLN" to="USD" amount={100} />);
       });
+    it('should render proper info about conversion when PLN -> USD', () => {
+        // render component
+        render(<ResultBox from="PLN" to="USD" amount={100} />);
+
+        // find main div
+        const resultField = screen.getByTestId('result');
+
+        // check if output = input
+        expect(resultField).toHaveTextContent('PLN 100.00 = $28.57');
+
+ 
+    });
 });
