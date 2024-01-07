b22e3d4f01bc37f12f55c1e6b228d2ba3dccade7
Julia Glinka <julia.w.glinka@gmail.com>
2024-01-07 16:51:34 +0100

    Add more data to check in previous ResultBox test

--- src/components/ResultBox/ResultBox.test.js
+++ src/components/ResultBox/ResultBox.test.js
@@ -1,5 +1,5 @@
 import ResultBox from './ResultBox';
-import { render, screen } from '@testing-library/react';
+import { render, screen, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 
@@ -7,16 +7,31 @@ describe('Component ResultBox', () => {
     it('should render without crashing', () => {
         render(<ResultBox from="PLN" to="USD" amount={100} />);
       });
-    it('should render proper info about conversion when PLN -> USD', () => {
-        // render component
-        render(<ResultBox from="PLN" to="USD" amount={100} />);
+    describe('should render proper info about conversion when PLN -> USD', () => {
 
-        // find main div
-        const resultField = screen.getByTestId('result');
+        const testCases = [
+            { amount: '100', result: 'PLN 100.00 = $28.57' },
+            { amount: '20', result: 'PLN 20.00 = $5.71' },
+            { amount: '200', result: 'PLN 200.00 = $57.14' },
+            { amount: '345', result: 'PLN 345.00 = $98.57' },
+        ];
 
-        // check if output = input
-        expect(resultField).toHaveTextContent('PLN 100.00 = $28.57');
+        for(const testObj of testCases) {
+            const amountAsNum = parseInt(testObj.amount);
 
- 
+            it('test', () => {
+                // render component
+                render(<ResultBox key={testObj.amount} from="PLN" to="USD" amount={amountAsNum} />);
+
+                // find main div
+                const resultField = screen.getByTestId('result');
+
+                // check if output = input
+                expect(resultField).toHaveTextContent(testObj.result);
+
+                // unmount component
+                cleanup();
+            });
+        };
     });
 });
